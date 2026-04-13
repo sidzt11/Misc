24a6cbe808645605a7e9838a3b33e56b680160d7
sidzt11 <sidzt1811@gmail.com>
2026-04-13 21:47:20 +0530

    Arrays : 3

---
+++ Java/Arrays/RotateMatrixBy90.java
@@ -0,0 +1,22 @@
+// ________________________________________________________
+// Rotate Matrix by 90 degrees
+// ________________________________________________________
+
+class Solution {
+    public void rotateMatrix(int[][] mat) {
+        int n = mat.length;
+
+        for (int i = 0; i < n / 2; i++) {                
+            for (int j = i; j < n - 1 - i; j++) {          
+                
+                int temp = mat[i][j];
+
+                mat[i][j] = mat[n - 1 - j][i];
+                mat[n - 1 - j][i] = mat[n - 1 - i][n - 1 - j];
+                mat[n - 1 - i][n - 1 - j] = mat[j][n - 1 - i];
+                mat[j][n - 1 - i] = temp;
+            }
+        }
+    }
+}
+
