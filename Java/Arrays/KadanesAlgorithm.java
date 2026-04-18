f80f69672feedf9843cd7d0498bf4d0cf338b225
sidzt11 <sidzt1811@gmail.com>
2026-04-18 22:41:00 +0530

    kadane

---
+++ Java/Arrays/KadanesAlgorithm.java
@@ -0,0 +1,16 @@
+// ________________________________________________________
+// Kadanes Algorithm - Maximum Subarray Sum
+// ________________________________________________________
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, max = nums[0];
+
+        for (int n : nums) {
+            sum += n;
+            max = Math.max(max, sum);
+            if (sum < 0) sum = 0;
+        }
+        return max;
+    }
+}
