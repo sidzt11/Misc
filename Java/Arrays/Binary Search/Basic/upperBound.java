9dd5c45dcc83735e44b12bbd17d88bacfd1a7b36
sidzt11 <sidzt1811@gmail.com>
2026-06-24 23:58:35 +0530

    upper bound

---
+++ Java/Arrays/Binary Search/Basic/upperBound.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int upperBound(int[] nums, int x) {
+
+        int l = 0;
+        int r = nums.length - 1;
+        int ans = nums.length;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > x) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
